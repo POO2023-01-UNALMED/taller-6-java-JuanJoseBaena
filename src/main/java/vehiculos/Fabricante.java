2f5a8a71ea6f622c49def29f0379dde98f04d9be
JuanJoseBaena <124928411+JuanJoseBaena@users.noreply.github.com>
2023-03-25 21:45:23 -0500

    Commit 2

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -8,4 +8,53 @@ public class Fabricante{
 	String nombre;
 	Pais pais;
 	static ArrayList<Fabricante> fabricantes = new ArrayList<Fabricante>();
+	
+	public Fabricante (String nombre, Pais pais) {
+		this.nombre = nombre;
+		this.pais = pais;
+		fabricantes.add(this);
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre ;
+	}
+	
+	
+	public Pais getPais() {
+		return this.pais;
+	}
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+	
+	
+	public static Fabricante fabricaMayorVentas() {
+		//HashMap<String, Interger> cont newHashMap<String, Interger>();
+		for (Fabricante fabricante: fabricantes) {
+			//if (cont.containsKey(fabricante.getNombre())){
+			cont.put(fabricante.getNombre(), cont.get(fabricante.getNombre())+1);
+		}else {
+			cont.put(fabricante.getNombre(), 1);
+		}
+	}
+	
+	int mayor = 0;
+	String nombreFabricante = "";
+	for (Map.Entry<String, Interger> entry : cont.entrySet()){
+		if(entry.getValue() > mayor) {
+			mayor = entry.getValue();
+			nombreFabricante = entry.getKey();
+		}
+	}
+	for(Fabricante fabricante : fabricantes) {
+		if (fabricantes.getNombre().equals(nombreFabricante)) {
+			return fabricante;
+		}
+	}
+	return null;
+	
+			
 }
